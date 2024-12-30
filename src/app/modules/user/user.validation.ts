b26b2fcaed048cfd9347f533dae831df316d1c1c
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-12-31 00:26:39 +0600

    some code update

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -3,16 +3,12 @@ import { z } from 'zod';
 const createUserValidationSchema = z.object({
   body: z.object({
     name: z.string().nonempty('Name is required'),
-    email: z
-      .string()
-      .email('Invalid email address')
-      .nonempty('Email is required'),
+    email: z.string().email('Invalid email address'),
     password: z
       .string({
         invalid_type_error: 'Password must be string',
       })
-      .min(8, 'Password must be at least 8 characters long')
-      .nonempty('Password is required'),
+      .min(8, 'Password must be at least 8 characters long'),
   }),
 });
 const updateUserValidationSchema = z.object({
